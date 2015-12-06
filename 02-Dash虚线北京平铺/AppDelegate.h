5653db7de1ac1abf76d23c3941fa44b2a6c64485
zhangguangchao <976325486@qq.com>
2015-12-06 11:42:50 +0800

    Initial Commit

---
+++ 02-Dash虚线北京平铺/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02-Dash虚线北京平铺
+//
+//  Created by 闲人 on 15/12/6.
+//  Copyright © 2015年 闲人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02-Dash虚线北京平铺/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02-Dash虚线北京平铺
+//
+//  Created by 闲人 on 15/12/6.
+//  Copyright © 2015年 闲人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
